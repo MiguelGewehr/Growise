d3dba3063164dc140c2cc579b1f7eb563bed186f
Milena <milenacurtinhas@gmail.com>
2024-07-01 13:18:01 -0300

    Mudanças na integracao do login e cadastro de produtos

--- assets/js/cadastro_produto.js
+++ assets/js/cadastro_produto.js
@@ -1,47 +1,47 @@
-document.getElementById('cadastro-form').addEventListener('submit', async function (event) {
-    event.preventDefault();
+let botao = document.getElementById('botaoCadastrar');
 
-    const nome = document.getElementById('nome').value;
-    const precoCompra = document.getElementById('preco_compra').value;
-    const estoque = document.getElementById('estoque').value;
-    const marca = document.getElementById('marca').value;
-    const fornecedor = document.getElementById('fornecedor').value;
-    const precoVenda = document.getElementById('preco_venda').value;
-    const quantidade = document.getElementById('quantidade').value;
-    const descricao = document.getElementById('descricao').value;
+if(botao){
+    addEventListener('click', async function (event) {
+
+        const nome = document.getElementById('nome').value;
+        const precoCompra = document.getElementById('preco_compra').value;
+        const estoque = document.getElementById('estoque').value;
+        const marca = document.getElementById('marca').value;
+        const fornecedor = document.getElementById('fornecedor').value;
+        const precoVenda = document.getElementById('preco_venda').value;
+        const quantidade = document.getElementById('quantidade').value;
+        const descricao = document.getElementById('descricao').value;
 
 
-    const data = {
-        nome: nome,
-        precoCompra: precoCompra,
-        estoque: estoque,
-        marca: marca,
-        fornecedor: fornecedor,
-        precoVenda: precoVenda,
-        quantidade: quantidade,
-        descricao: descricao
-    };
+        const data = {
+            nome: nome,
+            precoCompra: precoCompra,
+            estoque: estoque,
+            marca: marca,
+            fornecedor: fornecedor,
+            precoVenda: precoVenda,
+            quantidade: quantidade,
+            descricao: descricao
+        };
 
-    try {
-        //verificar se esse tipo de envio de dados está bom
-        const response = await fetch('URL_DO_SEU_BACKEND', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+        try {
+            const response = await fetch('URL_DO_SEU_BACKEND', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
 
-        if (response.ok) {
-            const result = await response.json();
-            alert('Cadastro realizado com sucesso!');
-            //console.log(result);
-        } else {
+            if (response.ok) {
+                const result = await response.json();
+                alert('Cadastro realizado com sucesso!');
+            } else {
+                alert('Erro ao cadastrar!');
+            }
+        } catch (error) {
             alert('Erro ao cadastrar!');
-            //console.error('Erro:', response.statusText);
         }
-    } catch (error) {
-        alert('Erro ao cadastrar!');
-        //console.error('Erro:', error);
-    }
-});
+    });
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,28 +1,49 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+let botao = document.getElementById('botaoEntrar');
 
-    fetch('/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, senha })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.role === 'vendedor') {
-            window.location.href = 'vendas.html';
-        } else if (data.role === 'gerente') {
-            window.location.href = 'dashboard.html';
-        } else {
-            alert('Usuário ou senha inválidos.');
+if(botao){
+    botao.addEventListener('click', async function() {
+
+        const login = document.getElementById('email').value;
+        const password = document.getElementById('senha').value;
+
+        const data = {
+            login: login,
+            password: password
+        };
+
+        try{
+            let response = await fetch('http://localhost:8080/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            
+            if (response.ok) {
+                const data = await response.json();
+                console.log(response); // true
+                const token = data.token;
+
+                acessToken = 'Bearer ' + token;
+                localStorage.setItem('accessToken', token);
+                console.log('Usuário cadastrado com sucesso!');
+
+                if (data.role === 'vendedor') {
+                    window.location.href = 'vendas.html';
+                } else if (data.role === 'gerente') {
+                    window.location.href = 'dashboard.html';
+                }
+            } else {
+                console.log('Erro:', response.statusText);
+                alert('Usuário ou senha inválidos.');
+            }
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Erro ao fazer login. Tente novamente mais tarde.');
+        catch(error) {
+            console.error('Error:', error);
+            alert('Erro ao fazer login. Tente novamente mais tarde.');
+        };
     });
-});
+}
