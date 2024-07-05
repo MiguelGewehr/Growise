86900f8e8901c670a290503cbbe863847400b4e6
Miguel Gewehr de Oliveira <miguel.gewehr.dev@gmail.com>
2024-07-05 09:08:21 -0300

    Ajeitando ultimos detalhes da pagina de vendas, agora só precisa integrar direitinho com o backend

--- assets/js/vendas.js
+++ assets/js/vendas.js
@@ -13,13 +13,15 @@ function adicionarProduto(classeDadosProduto) {
 
     var preco = produto.querySelector('.preco').textContent;
 
+    var valor = convertToFloat(preco) * quantidade;
+
 
     // Criando a estrutura HTML
     var novoProduto = `
         <div class="ms-4">
             <p>`+ nome +`</p>
             <p>Quantidade: ` + quantidadeTexto + `</p>
-            <p>Valor: R$ ` + preco + `</p>
+            <p>Valor: R$` + valor + `</p>
         </div>
         <hr>
     `;
@@ -29,3 +31,33 @@ function adicionarProduto(classeDadosProduto) {
     var container = document.querySelector('.container-pedidos');
     container.insertAdjacentHTML('beforeend', novoProduto);
 }
+
+function convertToFloat(currencyStr) {
+    // Remove o símbolo de moeda e espaços em branco
+    let numberStr = currencyStr.replace('R$', '').trim();
+    // Substitui a vírgula por um ponto
+    numberStr = numberStr.replace(',', '.');
+    // Converte a string para float
+    let numberFloat = parseFloat(numberStr);
+
+    return numberFloat;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    const maxNumber = 10; // Você pode mudar esse valor para quantos números quiser
+
+    for (let i = 1; i <= maxNumber; i++) {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.className = 'dropdown-item';
+        a.href = '#';
+        a.textContent = i;
+        a.addEventListener('click', function() {
+            document.getElementById('dropdownMenuButton1').textContent = i;
+        });
+        li.appendChild(a);
+        dropdownMenu.appendChild(li);
+    }
+});
