d8f1dbd80d85755662ed49d977e76924c700bb85
Aurea <aureasoliveiraa@gmail.com>
2024-07-28 01:47:17 -0300

    db connection + spring initial config

---
+++ backend/api/src/main/java/com/growise/api/GrowiseApplication.java
@@ -0,0 +1,13 @@
+package com.growise.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowiseApplication.class, args);
+	}
+
+}
---
+++ backend/api/src/test/java/com/growise/api/GrowiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
